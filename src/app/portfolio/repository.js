f5f3075b0a538135b39c1d3649b9b63e67d6369a
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-08-08 15:19:39 +0700

    modify api for delete portfolio

--- src/app/portfolio/repository.js
+++ src/app/portfolio/repository.js
@@ -2,95 +2,105 @@ import db from "../../core/utils/db.js";
 
 export default class PortfolioRepository {
 
-    static async create(userId, data){
+    static async create(userId, data) {
         return await db.portfolio.create({
-            data : {
-                userId : userId,
-                name : data.name,
-                repoUrl : data.repo_url,
-                demoUrl : data.demo_url,
-                description : data.description
+            data: {
+                userId: userId,
+                name: data.name,
+                repoUrl: data.repo_url,
+                demoUrl: data.demo_url,
+                description: data.description
             },
-            select : {
-                id : true
+            select: {
+                id: true
             }
         })
     }
 
-    static async get_my_portfolio(userId){
+    static async get_my_portfolio(userId) {
         return await db.portfolio.findMany({
-            where : {
-                userId : userId
+            where: {
+                userId: userId
             },
-            select : {
-                id : true,
-                name : true,
-                repoUrl : true,
-                demoUrl : true
+            select: {
+                id: true,
+                name: true,
+                repoUrl: true,
+                demoUrl: true
             }
         })
     }
 
-    static async get_detail(id, userId){
+    static async get_detail(id, userId) {
         return await db.portfolio.findUnique({
-            where : {
+            where: {
                 id: id,
-                userId : userId
+                userId: userId
             },
-            include : {
-                portfolioImages : true
+            include: {
+                portfolioImages: true
             }
         })
     }
 
-    static async find_data(id){
+    static async find_data(id) {
         return await db.portfolio.findUnique({
-            where : {
-                id : id
+            where: {
+                id: id
             },
-            select : {
-                id : true,
-                userId : true
+            include: {
+                portfolioImages: true
             }
         })
     }
 
-    static async update_portfolio(id, data){
+    static async update_portfolio(id, data) {
         return await db.portfolio.update({
-            where : {
-                id : id
+            where: {
+                id: id
             },
-            data : {
-                name : data.name,
-                repoUrl : data.repo_url,
-                demoUrl : data.demo_url,
-                description : data.description
+            data: {
+                name: data.name,
+                repoUrl: data.repo_url,
+                demoUrl: data.demo_url,
+                description: data.description
             },
-            select : {
-                id : true
+            select: {
+                id: true
             }
         })
     }
 
-    static async find_user(userId){
+    static async find_user(userId) {
         return await db.user.findUnique({
-            where : {
-                id : userId
+            where: {
+                id: userId
             },
-            select : {
-                id : true,
-                passwordHash : true
+            select: {
+                id: true,
+                passwordHash: true
             }
         })
     }
 
-    static async delete_portfolio(id){
+    static async delete_portfolio(id) {
         return await db.portfolio.delete({
-            where : {
-                id : id
+            where: {
+                id: id
             },
-            select : {
-                id : true
+            select: {
+                id: true
+            }
+        })
+    }
+
+    static async delete_image(imageId) {
+        return await db.portfolioImage.delete({
+            where: {
+                id: imageId
+            },
+            select: {
+                id: true,
             }
         })
     }
--- src/app/portfolio/service.js
+++ src/app/portfolio/service.js
@@ -1,26 +1,27 @@
 import PortfolioRepository from "./repository.js";
 import ResponseError from "../../core/utils/response-error.js";
+import { deletePortfolioImage } from "../../core/middleware/portfolio-image.js";
 import bcrypt from "bcrypt";
 
 export default class PortfolioService {
 
-    static async Create(userId, data){
+    static async Create(userId, data) {
         const response = await PortfolioRepository.create(userId, data);
         return response;
     }
 
-    static async GetMyPortfolio(userId){
+    static async GetMyPortfolio(userId) {
         const response = await PortfolioRepository.get_my_portfolio(userId);
         return response;
     }
 
-    static async GetDetail(id, userId){
+    static async GetDetail(id, userId) {
         const response = await PortfolioRepository.get_detail(id, userId);
         if (!response) throw new ResponseError(404, "portfolio not found");
         return response;
     }
 
-    static async Update(id, userId, data){
+    static async Update(id, userId, data) {
         const find_data = await PortfolioRepository.find_data(id);
         if (!find_data) throw new ResponseError(404, "portfolio not found");
         if (find_data.userId !== userId) throw new ResponseError(401, "unauthorized");
@@ -29,7 +30,7 @@ export default class PortfolioService {
         return response;
     }
 
-    static async Delete(id, userId, data){
+    static async Delete(id, userId, data) {
         const find_data = await PortfolioRepository.find_data(id);
         if (!find_data) throw new ResponseError(404, "portfolio not found");
         if (find_data.userId !== userId) throw new ResponseError(401, "unauthorized");
@@ -37,7 +38,15 @@ export default class PortfolioService {
         const find_user = await PortfolioRepository.find_user(userId);
         const is_match = await bcrypt.compare(data.password, find_user.passwordHash);
         if (!is_match) throw new ResponseError(401, "wrong password");
-        
+
+        if (find_data.portfolioImages.length > 0) {
+            for (let i = 0; i < find_data.portfolioImages.length; i++) {
+                const delete_image = deletePortfolioImage(find_data.portfolioImages[i].fileName);
+                if (!delete_image) throw new ResponseError(500, delete_image.message);
+                await PortfolioRepository.delete_image(find_data.portfolioImages[i].id);
+            }
+        }
+
         const response = await PortfolioRepository.delete_portfolio(id);
         return response;
     }
