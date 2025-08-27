ce5d161532e3d06d49bc1b32339f9c857d3f8450
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-08-27 19:16:49 +0700

    setup middleware for error handle and not found

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
 // import dependencies
 import express from 'express';
 
+
+// import middleware
+import ErrorMiddlewar from "@/middleware/error-middleware";
+import NotFoundMiddleware from "@/middleware/not-found-middleware";
+
+
 // initialize express
 const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// middleware
+app.use(ErrorMiddlewar);
+app.use(NotFoundMiddleware);
+
 
 // export app
 export default app;
