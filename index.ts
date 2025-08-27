5737e9f7012a216a8023e05f49d40d33e980d93e
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-08-27 18:35:00 +0700

    setup express

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import 'dotenv/config';     // Load environment variables
+import app from "@/app";    // Import the Express app
+
+// Get environment variables
+const HOSTNAME = process.env.APP_HOSTNAME || "localhost";
+const PORT = process.env.APP_PORT || 3000;
+const URL = `${HOSTNAME}:${PORT}`;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${URL}`);
+});
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+// import dependencies
+import express from 'express';
+
+// initialize express
+const app = express();
+
+// export app
+export default app;
